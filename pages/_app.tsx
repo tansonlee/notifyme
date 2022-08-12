008c74baec081008b1f5e2a191352f8afc8d7eee
Tanson Lee <leetanson@gmail.com>
2022-08-11 23:54:37 -0700

    🔒 Working login system.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider, createStandaloneToast } from '@chakra-ui/react';
+
+const { ToastContainer } = createStandaloneToast();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider>
+			<Component {...pageProps} />
+			<ToastContainer />
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
